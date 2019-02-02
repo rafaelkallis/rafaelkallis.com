44526e180b978198babe6446e851b0e62c7828f9
Rafael Kallis <rk@rafaelkallis.com>
2019-02-02 13:09:52 +0100

    updated absract

--- src/component/Body/Abstract/index.js
+++ src/component/Body/Abstract/index.js
@@ -12,16 +12,33 @@ export default function Abstract(props) {
       className={'abstract-text ' + (props.className ? props.className : '')}>
       <h3>Abstract</h3>
       <p>
+        Blockchain engineer at <a
+          href="https://heroesgroup.ch"
+          target="_blank"
+          rel="noreferrer noopener">
+          Heroes Group
+        </a> (present, 2019).
+      </p>
+      <p>
+        Data scientist and engineer at{' '}
+        <a
+          href="https://valdon.de"
+          target="_blank"
+          rel="noreferrer noopener">
+          Valdon Mesh
+        </a> (2 months, 2018).
+      </p>
+      <p>
         Software engineer at{' '}
         <a
           href="https://covee.network"
           target="_blank"
           rel="noreferrer noopener">
           Covee Network
-        </a>.
+        </a> (4 months, 2018).
       </p>
       <p>
-        Master's degree in Computer Science at the{' '}
+        Pursuing Master's degree in Computer Science at the{' '}
         <a
           href="http://www.ifi.uzh.ch/en.html"
           target="_blank"
@@ -29,14 +46,22 @@ export default function Abstract(props) {
           University of Zurich
         </a>.
       </p>
+      <h3>Side Gigs</h3>
       <p>
-        I work on a daily basis on a node.js backend with
-        relational databases and solidity smart contracts.
-        I love designing large scale system/blockchain architectures.
+        <a
+          href="https://github.com/rafaelkallis/ticket-tagger"
+          target="_blank"
+          rel="noreferrer noopener">
+          Ticket Tagger
+        </a>, machine learning driven issue classification bot for GitHub.
       </p>
       <p>
-        During the weekend, I'm building  my own database system (from scratch).
-        Occasionaly, I read some database and distributed systems literature.
+        <a
+          href="https://github.com/rafaelkallis/adaptive-radix-tree"
+          target="_blank"
+          rel="noreferrer noopener">
+          The Adaptive Radix Tree
+        </a>, an adaptive radix tree for efficient indexing in main memory.
       </p>
     </div>
   );
